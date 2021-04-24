6a39b7572642ce67c0403d7470249644a5b1c714
nivesh38 <niveshjuggessar38@gmail.com>
2021-04-24 22:39:39 +0200

    test01

--- Game/Program.cs
+++ Game/Program.cs
@@ -18,25 +18,59 @@ namespace Game
 
         private static void makeMainMenu()
         {
+            string userValue;
+            bool inptvaled;
+            NewMethod(out userValue, out inptvaled);
+
+            Console.WriteLine($"You chose - {userValue}");
+            while (!inptvaled)
+            {
+                if (userValue == "s" || userValue == "l" || userValue == "c")
+                {
+                    Console.WriteLine("00");
+                    if (userValue == "s")
+                    {
+                        startGame();
+                    }
+                    else if (userValue == "c")
+                    {
+                        createChrater();
+                    }
+                    else if (userValue == "l")
+                    {
+                        loadGame();
+                    }
+
+                    inptvaled = true;
+                }
+                else
+                {
+
+                    Console.WriteLine("Try again");
+                    mainMenuOptions();
+                    userValue = Console.ReadLine();
+                }
+            }
+
+        }
+
+        private static void mainMenuOptions()
+        {
             Console.WriteLine("\n\n (S)tart new game");
             Console.WriteLine("(L)oad game");
             Console.WriteLine("(C)reate game");
-            var userValue = Console.ReadLine();
+        }
 
-            Console.WriteLine($"You chose - {userValue}");
+        private static void NewMethod(out string userValue, out bool inptvaled)
+        {
+            mainMenuOptions();
+            userValue = Console.ReadLine();
+            inptvaled = false;
+        }
 
-            if (userValue == "s" || userValue == "l" || userValue == "c") 
-            {
-                Console.WriteLine("00");
-            }
-            else
-            {
-                Console.WriteLine("Try again");
-                Console.WriteLine("\n\n (S)tart new game");
-                Console.WriteLine("(L)oad game");
-                Console.WriteLine("(C)reate game");
-                var userValu = Console.ReadLine();
-            }
+        private static void loadGame()
+        {
+            
         }
 
         private static void makeTitle()
@@ -44,6 +78,16 @@ namespace Game
             Console.BackgroundColor = ConsoleColor.Red;
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("GAME!!!");
+        }
+
+        private static void startGame() 
+        {
+            Console.WriteLine("you started a game");
+
+        }
+        private static void createChrater()
+        {
+            Console.WriteLine("you are creating a chrater");
         }
     }
 }
